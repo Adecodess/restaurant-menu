171063941ca8a0e7cf3d7f89e6342899b64b1b38
codernaija11 <66638496+codernaija11@users.noreply.github.com>
2020-12-30 14:26:09 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,104 @@
+const menu = [
+  {
+    id: 1,
+    title: "burger",
+    category: "breakfast",
+    price: 10.5,
+    img: "./images/item-1.jpg",
+    desc:
+      "A hamburger (also burger for short) is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, grilled, smoked or flame broiled. ",
+  },
+  {
+    id: 2,
+    title: "fried chicken",
+    category: "lunch",
+    price: 16.56,
+    img: "./images/item-2.jpg",
+    desc:
+      "When it comes to chicken there just isn't anything more delicious than a juicy crusty piece of finger-licking.",
+  },
+  {
+    id: 3,
+    title: "sharwarma",
+    category: "breakfast",
+    price: 11.7,
+    img: "./images/item-3.jpg",
+    desc:
+      " Shawarma is thinly sliced cuts of meat, like chicken, beef, goat, lamb, and sometimes turkey rolled into a large piece ",
+  },
+  {
+    id: 4,
+    title: "pizza",
+    category: "lunch",
+    price: 13.1,
+    img: "./images/item-4.jpg",
+    desc:
+      "Pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature",
+  },
+  {
+    id: 5,
+    title: "fried rice & chicken",
+    category: "breakfast",
+    price: 16.69,
+    img: "./images/item-5.jpg",
+    desc:
+      "Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat. ... As a homemade dish, fried rice is typically made with ingredients left over from other dishes, leading to countless variations.",
+  },
+  {
+    id: 6,
+    title: "chicken curry sauce",
+    category: "lunch",
+    price: 15.55,
+    img: "./images/item-6.jpg",
+    desc:
+      "A typical curry from the Indian subcontinent consists of chicken stewed in an onion- and tomato-based sauce, flavoured with ginger, garlic, tomato puree, chilli peppers and a variety of spices, often including turmeric, cumin, coriander, cinnamon, and cardamom.",
+  },
+  {
+    id: 7,
+    title: "efo riro & pounded",
+    category: "breakfast",
+    price: 10.5,
+    img: "./images/item-7.jpg",
+    desc:
+      "Pounded Yam is a staple in many West African homes, but it is not usually eaten by itself, it is often paired with many delicious African soups, and stews like Egusi, okra soup, Jute leaves soup (Ewedu), or Stewed Spinach (Efo Riro).",
+  },
+  {
+    id: 8,
+    title: "pasta",
+    category: "lunch",
+    price: 17.5,
+    img: "./images/item-8.jpg",
+    desc:
+      "pasta (US: /ˈpɑːstə/, UK: /ˈpæstə/; Italian pronunciation: [ˈpasta]) is a type of food typically made from an unleavened dough of wheat flour mixed with water or eggs, and formed into sheets or other shapes, then cooked by boiling or baking.",
+  },
+  {
+    id: 9,
+    title: "spaghetti",
+    category: "breakfast",
+    price: 12.5,
+    img: "./images/item-9.jpeg",
+    desc:
+      "Spaghetti (Italian: [spaˈɡetti]) is a long, thin, solid, cylindrical noodle pasta. It is a staple food of traditional Italian cuisine. Like other pasta, spaghetti is made of milled wheat and water and sometimes enriched with vitamins and minerals. Italian spaghetti is typically made from durum wheat semolina.",
+  },
+  {
+    id: 10,
+    title: "chicken & chips",
+    category: "lunch",
+    price: 18.5,
+    img: "./images/item-10.jpg",
+    desc:
+      "It consists of a piece of fried, roasted, or barbecued chicken and chips, or French fries as they are known in the U.S. The consumption of chicken and chips is a popular food choice based on its value for money. ... Other names for chicken and chips include: Chicken fingers, chicken tenders, chicken nuggets and fries.",
+  },
+  {
+    id: 11,
+    title: "milkshake",
+    category: "shakes",
+    price: 10.99,
+    img: "./images/item-11.jpg",
+    desc:
+      "A milkshake, or simply shake, is a drink that is usually made by blending milk, ice cream, and flavorings or sweeteners such as butterscotch, caramel sauce, chocolate syrup, fruit syrup, or whole fruit into a thick, sweet, cold mixture.",
+  },
+ 
+];
+
+export default menu;
